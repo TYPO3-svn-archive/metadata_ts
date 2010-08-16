a85f8c0601c469cdd1cadd630a761812d5171629
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-16 19:00:03 +0000

    Updated md5 checksum
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/metadata_ts/trunk@36860 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "metadata_ts"
+# Extension Manager/Repository config file for ext "metadata_ts".
 #
-# Auto generated 17-03-2009 19:01
+# Auto generated 16-08-2010 20:59
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"e4ca";s:10:"README.txt";s:4:"3935";s:21:"ext_conf_template.txt";s:4:"70c1";s:12:"ext_icon.gif";s:4:"5807";s:17:"ext_localconf.php";s:4:"253c";s:32:"api/class.user_metadata_cobj.php";s:4:"88b8";s:14:"doc/manual.sxw";s:4:"3016";s:18:"samples/wbtc05.jpg";s:4:"b9a7";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"4328";s:10:"README.txt";s:4:"3935";s:21:"ext_conf_template.txt";s:4:"70c1";s:12:"ext_icon.gif";s:4:"5807";s:17:"ext_localconf.php";s:4:"253c";s:32:"api/class.user_metadata_cobj.php";s:4:"c5e5";s:14:"doc/manual.sxw";s:4:"3016";s:18:"samples/wbtc05.jpg";s:4:"52b1";}',
 	'suggests' => array(
 	),
 );
